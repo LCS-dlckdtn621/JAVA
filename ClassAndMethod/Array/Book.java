f1b80ea351a83078d3729f0a1afe27abd4b7f584
changsooooooooooo <lcs940621@gmail.com>
2021-05-29 13:29:51 +0900

    before deepcopy

---
+++ ClassAndMethod/Array/Book.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class Book {
+
+    private String title;
+    private String author;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+    }
+
+    public String showInfo(){
+        return title+" 는 "+author+"가 집필하였습니다.";
+    }
+
+}
