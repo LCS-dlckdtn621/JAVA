5c864d177c8ac90a9e28204796da7a90dd0d89a1
changsooooooooooo <lcs940621@gmail.com>
2021-05-29 16:32:43 +0900

    downcasting sample

---
+++ ClassAndMethod/downcasting/AnimalTest.java
@@ -0,0 +1,47 @@
+package downcasting;
+
+import polymorphism.Animal;
+import polymorphism.Eagle;
+import polymorphism.Human;
+import polymorphism.Tiger;
+
+import java.util.ArrayList;
+
+public class AnimalTest {
+    public static void main(String[] args){
+        polymorphism.Animal hAnimal = new Human();
+        polymorphism.Animal tAnimal = new Tiger();
+        polymorphism.Animal eAnimal = new Eagle();
+        //다형성에 해당!
+
+        ArrayList<polymorphism.Animal> animalList = new ArrayList<>();
+        animalList.add(hAnimal);
+        animalList.add(tAnimal);
+        animalList.add(eAnimal);
+
+        AnimalTest test = new AnimalTest();
+        for(polymorphism.Animal animal : animalList){
+            test.moveAnimal(animal);
+        }
+
+        Tiger tiger = (Tiger)tAnimal;
+        //downcasting으로 형변환 하고 새로 선언하는 것
+        if(tiger instanceof Tiger){
+            //보험 코드
+            tiger.hunt();
+        }
+
+        for(polymorphism.Animal animal : animalList){
+            if(animal instanceof Eagle){
+                Eagle eagle = (Eagle)animal;
+                eagle.flying();
+            }
+        }
+
+    }
+
+    public void moveAnimal(Animal animal){
+        animal.move();
+    }
+
+}
--- out/production/GitHub/polymorphism/Readme.md
+++ out/production/GitHub/polymorphism/Readme.md
@@ -1,1 +1,7 @@
 - Upcasting 하게 되면 개별 overiding된 method는 그냥 쓸 수 있지만 각자의 method는 사용하지 못한다.
+
+
+<상속 사용 예시>
+- 일반적인 것과 구체적인 관계에서 사용(IS-A)
+
+- 클래스가 다른 클래스를 포함 : Student  > Subject 와 같은 경우(HAS-A)
